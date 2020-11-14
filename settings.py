148b71cedda4cdeb7f6e977b169f123853c13114
John Oatis <johnoatis@pop-os.localdomain>
2020-11-14 11:46:10 -0500

    Added Bullets

--- settings.py
+++ settings.py
@@ -9,4 +9,12 @@ class Settings:
         self.bg_color = (230, 230, 230)
         # Ship Speed
         self.ship_speed = 1.5
+        # Bullet Settings 
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        
+
         
